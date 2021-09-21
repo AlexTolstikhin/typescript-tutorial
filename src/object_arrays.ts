884f04b6fdc344fd4c5ea97897a6efa9bf6e2da2
AlexTolstikhin <alexeytolstikhin@gmail.com>
2021-09-20 21:43:46 -0700

    ts config module

---
+++ src/object_arrays.ts
@@ -0,0 +1,25 @@
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
+    name: 'Alex',
+    age: 32,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+
+person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']; Causes error
+
+let favoriteActivities: string[];
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
